00cecf42b9192fe57348b1485986fe15c2c69481
rajat kumar <rajatko614@gmail.com>
2026-03-29 18:17:00 +0530

    [UPDATE]: mongodb intregation in AWS

--- src/main/java/com/digest/dertyu/JournalApplication.java
+++ src/main/java/com/digest/dertyu/JournalApplication.java
@@ -2,11 +2,6 @@ package com.digest.dertyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class JournalApplication {
