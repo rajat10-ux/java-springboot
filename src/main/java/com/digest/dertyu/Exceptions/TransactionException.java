799a65fe52e3bf200cad84e5a44d1329268c0e7b
rajat kumar <rajatko614@gmail.com>
2026-03-29 17:49:45 +0530

    [ADD]:(Transactions & TransactionExceptions),[UPDATE]: JournalService

---
+++ src/main/java/com/digest/dertyu/Exceptions/TransactionException.java
@@ -0,0 +1,7 @@
+package com.digest.dertyu.Exceptions;
+
+public class TransactionException extends RuntimeException{
+    public TransactionException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/digest/dertyu/Service/JournalEntryService.java
+++ src/main/java/com/digest/dertyu/Service/JournalEntryService.java
@@ -2,10 +2,12 @@ package com.digest.dertyu.Service;
 
 import com.digest.dertyu.Entitites.JournalEntity;
 import com.digest.dertyu.Entitites.User;
+import com.digest.dertyu.Exceptions.TransactionException;
 import com.digest.dertyu.Repository.JournalEntryRepository;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,18 +19,23 @@ public class JournalEntryService {
 
     private final  JournalEntryRepository journalEntryRepository;
     private final UserEntryService userEntryService;
+    @Transactional
     public JournalEntity createEntry(JournalEntity journalEntity, String name){
-        User byUserName = userEntryService.findByUserName(name);
-        journalEntity.setDate(LocalDateTime.now());
-        JournalEntity saved = journalEntryRepository.save(journalEntity);
-        byUserName.getJournalEntities().add(saved);
-        userEntryService.createEntry(byUserName);
-        return saved;
+        try {
+            User byUserName = userEntryService.findByUserName(name);
+            journalEntity.setDate(LocalDateTime.now());
+            JournalEntity saved = journalEntryRepository.save(journalEntity);
+            byUserName.getJournalEntities().add(saved);
+            userEntryService.createEntry(byUserName);
+            return saved;
+        } catch (Exception e) {
+            throw new TransactionException(e.getMessage());
+        }
     }
+
     public List<JournalEntity>getAllEntry(){
         return journalEntryRepository.findAll();
     }
-
 
     public Optional<JournalEntity>getByJournalId(ObjectId myId) {
         return journalEntryRepository.findById(String.valueOf(myId));
